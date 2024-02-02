85e7461228ed05ac3224a109b81ad74472a8a4fd
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2024-02-02 15:51:11 -0500

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+/* App.js */
+import React, { Component } from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+ 
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getRandomNumbers = (min, max) => {
+    const open = Math.random() * (max - min) + min;
+    const high = Math.random() * (max - open) + open;
+    const close = Math.random() * (high - open) + open;
+    const low = Math.random() * (min - close) + close;
+
+    return [open, high, close, low];
+  }
+
+	render() {
+
+
+    const today = new Date();
+    const dataPoints = [];
+    for(let i = 30; i > 0; i--){
+      dataPoints.push({ x: new Date().setDate(today.getDate() - i), y: this.getRandomNumbers(30, 52) });
+    }
+    
+		const options = {
+			theme: "dark2", // "light1", "light2", "dark1", "dark2"
+			animationEnabled: true,
+			exportEnabled: true,
+			title:{
+				text: "ABC Stock price"
+			},
+			axisX: {
+				valueFormatString: "MMM YY"
+			},
+			axisY: {
+				prefix: "$",
+				title: "Price (USD)"
+			},
+			data: [{
+				type: "candlestick",
+				showInLegend: true,
+				name: "ABC",
+				yValueFormatString: "$###0.00",
+				xValueFormatString: "MMMM YY",
+				dataPoints
+			}
+		  ]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				onRef={ref => this.chart = ref}
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
 }
-
-export default App;
+export default App;  
